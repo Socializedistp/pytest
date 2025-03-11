0644cd31dbfddd7c7622cfff86a32beb06b478c5
socializedistp <seolyein616@gmail.com>
2025-03-11 20:10:05 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository is for 2025 NCS application development class.
---
+++ week02.py
@@ -0,0 +1,13 @@
+# 1) ICE Ammericano: 2000 2) Cafe Latte: 3000
+while True:
+    menu = input("1) Ice Americano 2) Cafe Latte 3) Exit: ")
+    if menu == "1":
+        print("Ice Americano ordered. Rrice: 2000 won")
+        
+    elif menu == "2":
+        print("Cafe Latte ordered. Rrice: 3000 won")
+        
+    elif menu == "3":
+        print("Finish order")
+        break
+
