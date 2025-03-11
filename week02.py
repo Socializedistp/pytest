66fe2be1514f4c476f4153a1c0d08b5a323d0064
socializedistp <seolyein616@gmail.com>
2025-03-11 20:29:14 +0900

    week02 v0.4

--- week02.py
+++ week02.py
@@ -1,14 +1,17 @@
 # 1) ICE Ammericano: 2000 2) Cafe Latte: 3000
+drinks = ["ICE Ammericano", "Cafe Latte"]
 prices = [2000, 3000]
 
 while True:
-    menu = input("1) Ice Americano{prices[0]}won 2) Cafe Latte{prices[1]}won 3) Exit: ")
+    menu = input(f"1) {drinks[0]} {prices[0]}won 2) {drinks[1]} {prices[1]}won 3) Exit: ")
     if menu == "1":
-        print("Ice Americano ordered. Rrice: {prices[0]}won")
+        print(f"{drinks[0]} ordered. Rrice: {prices[0]}won")
         
     elif menu == "2":
-        print("Cafe Latte ordered. Rrice: {prices[1]}won")
+        print(f"{drinks[1]} ordered. Rrice: {prices[1]}won")
         
     elif menu == "3":
         print("Finish order")
         break
+    else:
+        print(f"{menu} menu is not exist. please choose above menu.")
